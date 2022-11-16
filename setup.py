ddaf721f058732981fdfe67a326a162f56863a5a
Juan Pedro Hurtado Masero <juan.hurtado@co.idom.com>
2022-11-16 15:59:24 +0100

    nuevas

--- setup.py
+++ setup.py
@@ -15,12 +15,11 @@ LONG_DESCRIPTION = (HERE / "README.md").read_text(encoding='utf-16')
 LONG_DESC_TYPE = "text/markdown"
 
 INSTALL_REQUIRES = [
-      'sys'
-      """,
+      'sys',
       'boto3',
       'json',
       'importlib',
-      'abc'"""
+      'abc'
       ]
 
 setup(
