6f7dea534b6fc9fda34f4cc50fd645bb2a63f3ce
Juan Pedro Hurtado Masero <juan.hurtado@co.idom.com>
2022-11-17 14:39:32 +0100

    version 0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 
 HERE = pathlib.Path(__file__).parent
-VERSION = '0.1.4'
+VERSION = '0.1.5'
 PACKAGE_NAME = 'crtmlib' 
 AUTHOR = 'Juan Pedro Hurtado Masero'
 AUTHOR_EMAIL = 'juanpepitt@gmail.com'
